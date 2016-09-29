5981a3dc2f202bfc836fe92d6effe7ad600213a8
castrofernandez <juancastrofernandez@iMac-de-Cristina.local>
2016-09-29 19:24:45 +0200

    New NUBE domain.

--- source/javascripts/CarrifyClient/CarrifyClient.js
+++ source/javascripts/CarrifyClient/CarrifyClient.js
@@ -1,5 +1,5 @@
 var  CarrifyClient = {
-  baseUrl: "//carrify.tech/api/v1",
+  baseUrl: "//nube.carrify.tech/api/v1",
   securityToken: "e2769d6f-61ec-4da6-8bd0-fae1116f5d04", // I know this should not be public but...
   locationId: 4,
   categories: {
